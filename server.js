c30ed0e04403779032add14777db4230b680a1e4
Trey Eckels <trey.eckels@gmail.com>
2021-09-26 15:43:21 -0400

    Initial commit.

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send("Helo World!")
+})
+
+app.listen(PORT, () => console.log('Now listening'))
